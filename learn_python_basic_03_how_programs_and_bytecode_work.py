2c55f006814c2e09ee66522fac8454b4a630e00f
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-02 10:53:50 +0700

    done: Learn Python [Basic] - 03 - How Programs and Bytecode Work

---
+++ learn_python_basic_03_how_programs_and_bytecode_work.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+# ini adalah contoh comment
+a = 10 # ini adalah comment juga
+print(a)
+""""
+ini adalah comment
+multi line
+"""
+
+# kita bisa mengcompile python
+# ke bentuk bytecode
+
+# cara mengcompile, buka terminal lalu ketik
+# python -m py_compile namafile.py
