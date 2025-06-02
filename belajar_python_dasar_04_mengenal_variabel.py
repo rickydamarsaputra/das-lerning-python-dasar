313661f448e8ee8135d8baad391490b29d8eba27
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-02 11:08:34 +0700

    done: Belajar Python [Dasar] - 04 - Mengenal Variabel

---
+++ belajar_python_dasar_04_mengenal_variabel.py
@@ -0,0 +1,27 @@
+# variable adalah tempat menyimpan data
+a = 10
+x = 5
+panjang = 1000 
+
+# pemanggilan pertama
+print("Nilai a =", a)
+print("Nilai x =", x)
+print("Nilai panjang =", panjang)
+
+# penamaan variable
+# (tidak boleh menggunakan spasi dan tidak boleh diawali dengan angka)
+# nilai y = tidak boleh
+# 10juta = tidak boleh
+
+nilai_y = 15
+juta10 = 10000000
+nilaiZ = 17.5
+
+# pemanggilan kedua
+print("Nilai a=", a)
+a = 7
+print("Nilai a=", a)
+
+# assignment indirect
+b = a
+print("Nilai b=", b)
