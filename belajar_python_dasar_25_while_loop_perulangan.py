dc5ae3c4d92677a07913394f4021c0bf38e988a6
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-03 14:54:28 +0700

    done: Belajar Python [Dasar] - 25 - While Loop (Perulangan)

---
+++ belajar_python_dasar_25_while_loop_perulangan.py
@@ -0,0 +1,15 @@
+# while loop
+
+# while kondisi:
+#   aksi
+
+# akhir dari program
+
+angka = 0
+print(f"angka sekarang -> {angka}")
+
+while angka < 5:
+  angka += 1
+  print(f"angka sekarang -> {angka}")
+  print("hello, world!")
+print("akhir dari program")
