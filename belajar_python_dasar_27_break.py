49e86aea5a2dc25bf80ff6e6b8363d2787e0be08
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-03 15:17:11 +0700

    done: Belajar Python [Dasar] - 27 - Break

---
+++ belajar_python_dasar_27_break.py
@@ -0,0 +1,15 @@
+# Break
+
+angka = 0
+
+while angka < 5:
+  angka += 1
+  print(f"angka sekarang -> {angka}")
+
+  if angka == 3:
+    print("nice!")
+    break
+
+  print("hello, world!")
+
+print("akhir program")
