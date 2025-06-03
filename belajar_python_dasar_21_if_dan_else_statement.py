f718ebbe3b5e5d77d4ad31c7349d7628942b2b18
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-03 13:50:30 +0700

    done: Belajar Python [Dasar] - 21 - IF dan ELSE Statement

---
+++ belajar_python_dasar_21_if_dan_else_statement.py
@@ -0,0 +1,16 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda?: ")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+if nama == "ricky":
+  print(f"Selamat datang {nama}")
+else:
+  print(f"Terima kasih {nama}")
