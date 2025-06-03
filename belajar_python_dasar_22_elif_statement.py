74b5ab26632dc6d352bb23e7880cad71d345b843
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-03 14:04:23 +0700

    done: Belajar Python [Dasar] - 22 - ELIF Statement

---
+++ belajar_python_dasar_22_elif_statement.py
@@ -0,0 +1,12 @@
+# ELIF = else if statement
+
+nama = input("Masukan nama anda: ")
+
+if nama == "ricky": # kondisi 1
+  print(f"Selamat siang {nama}") # aksi true 1
+elif nama == "rifki": # kondisi 2
+  print(f"Selamat malam {nama}") # aksi true 2
+else:
+  print("Siapa anda?") # aksi false
+
+print("Ini adalah akhir dari program")
