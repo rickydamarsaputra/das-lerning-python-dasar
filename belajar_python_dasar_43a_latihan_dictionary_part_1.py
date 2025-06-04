6b7ddb85bf9881b9ff57ff3c56c8633eb68c58b4
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-04 15:45:26 +0700

    done: Belajar Python [Dasar] - 43a - Latihan Dictionary Part 1

---
+++ belajar_python_dasar_43a_latihan_dictionary_part_1.py
@@ -0,0 +1,27 @@
+import datetime
+import os
+
+mahasiswa_template = {
+  'nama': 'nama',
+  'nim': '00000000',
+  'sks_lulus': 0,
+  'lahir': datetime.datetime(1111,1,11)
+}
+
+data_mahasiswa = {}
+
+# os.system("cls") # untuk windows
+os.system("clear")
+print(f"{'SELAMAT DATANG':^20}")
+print(f"{'DATA MAHASISWA':^20}")
+print("-"*20)
+
+mahasiswa = dict.fromkeys(mahasiswa_template.keys())
+mahasiswa['nama'] = input("Nama Mahasiswa: ")
+mahasiswa['nim'] = input("NIM Mahasiswa: ")
+mahasiswa['sks_lulus'] = int(input("SKS Lulus: "))
+TAHUN_LAHIR = int(input("Tahun Lahir (YYYY): "))
+BULAN_LAHIR = int(input("Bulan Lahir (1-12): "))
+TANGGAL_LAHIR = int(input("Tanggal Lahir (1-31): "))
+mahasiswa['lahir'] = datetime.datetime(TAHUN_LAHIR,BULAN_LAHIR,TANGGAL_LAHIR)
+print(mahasiswa)
