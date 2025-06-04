ce3ffcfceaed8e29a642ff7cf2e47a18c7e5d7c9
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-04 16:02:25 +0700

    update: Belajar Python [Dasar] - 43b - Latihan Dictionary Part 2

--- belajar_python_dasar_43a_latihan_dictionary_part_1.py
+++ belajar_python_dasar_43a_latihan_dictionary_part_1.py
@@ -1,5 +1,7 @@
 import datetime
 import os
+import string
+import random
 
 mahasiswa_template = {
   'nama': 'nama',
@@ -10,18 +12,41 @@ mahasiswa_template = {
 
 data_mahasiswa = {}
 
-# os.system("cls") # untuk windows
-os.system("clear")
-print(f"{'SELAMAT DATANG':^20}")
-print(f"{'DATA MAHASISWA':^20}")
-print("-"*20)
+while True:
+  # os.system("cls") # untuk windows
+  os.system("clear")
+  print(f"{'SELAMAT DATANG':^20}")
+  print(f"{'DATA MAHASISWA':^20}")
+  print("-"*20)
 
-mahasiswa = dict.fromkeys(mahasiswa_template.keys())
-mahasiswa['nama'] = input("Nama Mahasiswa: ")
-mahasiswa['nim'] = input("NIM Mahasiswa: ")
-mahasiswa['sks_lulus'] = int(input("SKS Lulus: "))
-TAHUN_LAHIR = int(input("Tahun Lahir (YYYY): "))
-BULAN_LAHIR = int(input("Bulan Lahir (1-12): "))
-TANGGAL_LAHIR = int(input("Tanggal Lahir (1-31): "))
-mahasiswa['lahir'] = datetime.datetime(TAHUN_LAHIR,BULAN_LAHIR,TANGGAL_LAHIR)
-print(mahasiswa)
+  mahasiswa = dict.fromkeys(mahasiswa_template.keys())
+  mahasiswa['nama'] = input("Nama Mahasiswa: ")
+  mahasiswa['nim'] = input("NIM Mahasiswa: ")
+  mahasiswa['sks_lulus'] = int(input("SKS Lulus: "))
+  TAHUN_LAHIR = int(input("Tahun Lahir (YYYY): "))
+  BULAN_LAHIR = int(input("Bulan Lahir (1-12): "))
+  TANGGAL_LAHIR = int(input("Tanggal Lahir (1-31): "))
+  mahasiswa['lahir'] = datetime.datetime(TAHUN_LAHIR,BULAN_LAHIR,TANGGAL_LAHIR)
+  
+  KEY = ''.join((random.choice(string.ascii_uppercase) for i in range(6))) 
+  data_mahasiswa.update({KEY: mahasiswa})
+
+  print(f"{'KEY':<6} {'Nama':<17} {'SKS':<3} {'Lahir':<10}")
+  print("-"*50)
+
+  for mahasiswa in data_mahasiswa:
+    KEY = mahasiswa
+    NAMA = data_mahasiswa[KEY]['nama']
+    NIM = data_mahasiswa[KEY]['nim']
+    SKS = data_mahasiswa[KEY]['sks_lulus']
+    LAHIR = data_mahasiswa[KEY]['lahir'].strftime('%x')
+
+    print(f"{KEY:<6} {NAMA:<17} {SKS:<3} {LAHIR:<10}")
+
+  print('\n')
+  is_done = input("Mau tambah lagi (y/n)?: ")
+
+  if is_done == "n":
+    break
+
+print("Akhir dari program, terima kasih!")
