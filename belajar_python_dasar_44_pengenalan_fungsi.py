ad01661df05dd5a0c683343bcb18232232694480
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-04 16:14:09 +0700

    done: Belajar Python [Dasar] - 44 - Pengenalan Fungsi

---
+++ belajar_python_dasar_44_pengenalan_fungsi.py
@@ -0,0 +1,19 @@
+'''Membuat fungsi'''
+
+def hello_world():
+  # fungsi menampilkan hello world
+  print("hello world")
+  print("hello world")
+  print("hello world")
+  print("hello world")
+  print("hello world")
+
+hello_world()
+hello_world()
+
+# fungsi() -> akan error
+def fungsi():
+  # pemanggilan fungsi harus setelah dibuat
+  print("ini adalah fungsi")
+
+fungsi()
