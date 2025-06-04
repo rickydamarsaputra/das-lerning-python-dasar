e134790b80cb90607228b60af08481388ac2da36
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-04 09:58:04 +0700

    done: Belajar Python [Dasar] - 38 - Dictionary

---
+++ belajar_python_dasar_38_dictionary.py
@@ -0,0 +1,19 @@
+# list -> array, mengakses data menggunakan index
+data_list = ["ricky", "damar", "saputra"]
+
+print(data_list[0])
+
+# dictionary (dict) -> associative array
+# identifier -> key
+
+data_dict = {
+  'cp': 'ricky',
+  'tg': 'damar',
+  'sa': 'saputra',
+  'nmbr': 2001,
+  'list': data_list
+}
+
+print(data_dict['cp'])
+print(data_dict['nmbr'])
+print(data_dict['list'])
