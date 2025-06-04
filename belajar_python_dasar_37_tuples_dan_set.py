ff6127483eb96c2802d2e2e8d71137a57fff0893
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-04 09:50:54 +0700

    done: Belajar Python [Dasar] - 37 - Tuples dan Set

---
+++ belajar_python_dasar_37_tuples_dan_set.py
@@ -0,0 +1,17 @@
+# list 
+data_list = [10,1,4,6,6] # menggunakan kurung siku
+print(data_list)
+
+# tuples
+data_tuples = (2,7,1,5,8)
+print(data_tuples)
+print(data_tuples[1])
+
+# tidak bisa dilakukan di tuples
+# data_tuples[1] = "ricky" -> tidak bisa
+# data_tuples.append(1) -> tidak bisa
+
+# sets (himpunan)
+data_sets = {10,2,5,6,2,8}
+print(data_sets)
+# print(data_sets[0]) -> tidak bisa
