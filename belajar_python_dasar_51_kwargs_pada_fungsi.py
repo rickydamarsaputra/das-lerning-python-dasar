f0f91a59891954a8f384c58c22b85d1b93721576
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-05 10:34:55 +0700

    done: Belajar Python [Dasar] - 51 - **kwargs pada fungsi

--- belajar_python_dasar_51_kwargs_pada_fungsi.py
+++ belajar_python_dasar_51_kwargs_pada_fungsi.py
@@ -7,7 +7,33 @@ def fungsi(nama,tinggi,berat):
 fungsi("ricky", 175, 66)
 
 def fungsi(**kwargs):
-  # fungsi biasa
-  print(kwargs)
+  # **kwargs menghasilkan dictionary
+  nama = kwargs["nama"]
+  tinggi = kwargs["tinggi"]
+  berat = kwargs["berat"]
 
-fungsi("alam", 175, 66)
+  print(f"{nama}, punya tinggi {tinggi} dan berat {berat}")
+
+fungsi(nama="alam", tinggi=175, berat=66)
+
+# studi kasus
+def math(*args, **kwargs):
+  output = 0
+
+  if kwargs["option"] == "tambah":
+    for angka in args:
+      output += angka
+  elif kwargs["option"] == "kali":
+    output = 1
+    for angka in args:
+      output *= angka
+  else:
+    print("tidak ada operasi")
+
+  return output
+
+hasil_jumlah = math(1,5,6,7,2,option="tambah")
+hasil_kali = math(1,5,6,7,2,option="kali")
+
+print(f"hasil jumlah: {hasil_jumlah}")
+print(f"hasil kali: {hasil_kali}")
