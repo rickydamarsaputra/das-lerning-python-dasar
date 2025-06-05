c32396c745dccf114ca74f106e49678dc6189745
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-05 13:39:16 +0700

    done: Belajar Python [Dasar] - 57 - __Init__.py pada Package

---
+++ belajar_python_dasar_57_initpy_pada_package/main_app.py
@@ -0,0 +1,24 @@
+import sains
+
+hasil_tambah = sains.matematika.tambah(2,4,5,6)
+print(f"hasil tambah: {hasil_tambah}")
+
+hasil_fisika = sains.fisika.gaya(90,3)
+print(f"hasil fisika: {hasil_fisika}")
+
+hasil_kali = sains.matematika.kali(9,4,2)
+print(f"hasil kali: {hasil_kali}")
+
+pangkat_3 = sains.matematika.pangkat(3)
+print(f"hasil pangkat: {pangkat_3(5)}")
+
+# from sains import *
+
+# hasil_tambah = matematika.basic.tambah(2,4,5,6)
+# print(f"hasil tambah: {hasil_tambah}")
+
+# hasil_fisika = fisika.gaya(90,3)
+# print(f"hasil fisika: {hasil_fisika}")
+
+# hasil_kali = matematika.basic.kali(9,4,2)
+# print(f"hasil kali: {hasil_kali}")
