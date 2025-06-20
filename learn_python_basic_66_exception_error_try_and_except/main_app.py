365587c5b6fe5a059ac626899e1343755abb1b89
rickydamarsaputra <retrocode.rc@gmail.com>
2025-06-20 15:01:01 +0700

    done: Learn Python [Basic] - 66 - Exception, Error, Try and Except

---
+++ learn_python_basic_66_exception_error_try_and_except/main_app.py
@@ -0,0 +1,41 @@
+# exception akan terjadi saat program
+# mengalami error saat runtime
+
+# contoh sederhana untuk menangkap exception
+# from math import nan
+
+# input_user = int(input("masukan angka: "))
+# hasil = nan
+
+# try:
+#   hasil = 10/input_user
+# except:
+#   print("input tidak boleh 0")
+
+# print(f"hasil: {hasil}")
+
+# contoh di aplikasi
+
+while(True):
+  angka = int(input("masukan angka pembagi: "))
+  try:
+    hasil = 10/angka
+    print(f"hasil: {hasil}")
+    is_done = input("lanjutkan (y/n)?: ")
+
+    if is_done == "n":
+      break
+  except:
+    print("pembagi nol, silahkan masukan input lagi")
+
+print("Akhir dari program")
+
+try:
+  with open("data.txt", "r") as file:
+    print(file.read())
+except:
+  print("file data.txt tidak ditemukan, membuat file baru")
+  with open("data.txt", "w", encoding="utf-8") as file:
+    file.write("file baru")
+
+print("akhir dari program 2")
